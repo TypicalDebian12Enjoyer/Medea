15961a1d7e651ac754e96c1bd45186d50f65a3bd
Igor Puścion <igor.puscion@gmail.com>
2025-06-17 12:15:53 +0200

    Delete srcs directory

--- srcs/apka.cpp
+++
@@ -1,8 +0,0 @@
-#include "okienko.h"
-#include <gtkmm/application.h>
-
-int main(int argc, char* argv[]){
-  auto app = Gtk::Application::create("medea.github.com");
-
-  return app -> make_window_and_run<Okienko>(argc, argv);
-}
--- srcs/okienko.cpp
+++
@@ -1,9 +0,0 @@
-#include "okienko.h"
-
-Okienko::Okienko(){
-
-}
-
-Okienko::~Okienko(){
-
-}
